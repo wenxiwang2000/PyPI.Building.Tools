474687b280819345a9243326175cb5eb9948a46e
wenxiwang2000 <wenxiwang2000@gmail.com>
2026-03-08 13:52:59 +0100

    Rename package and update dependencies in setup.py

--- wenxi_ai_tools_package/wenxi_ai_tools/setup.py
+++ wenxi_ai_tools_package/wenxi_ai_tools/setup.py
@@ -1,21 +1,24 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
+with open("README.md", "r") as f:
     description = f.read()
 
 setup(
-    name="wenxi-ai-tools",
+    name="wenxi-csv-app",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+
+    install_requires=[
+        "pandas",
+        "openpyxl"
+    ],
+
     entry_points={
         "console_scripts": [
-            "wenxi-ai-tools = wenxi_ai_tools.main:hello",
+            "wenxi-csv-app = wenxi_csv_app.main:run",
         ],
     },
+
     long_description=description,
     long_description_content_type="text/markdown",
-    author="Wenxi Wang",
-    description="A simple example Python package with a command line entry point.",
-    python_requires=">=3.9",
 )
